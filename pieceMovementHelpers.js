0d7cccd14b208513cd3547d38a8f7a5540a32f19
Benson Ninh <bensonninh@gmail.com>
2017-04-11 16:13:42 -0700

    complete bishop movement being done with bishop + rook movement consequently completes queen movement

--- pieceMovementHelpers.js
+++ pieceMovementHelpers.js
@@ -1,73 +1,163 @@
 var withinBounds = function(x, y) {
-  return (x > 8 || x < 1 || y > 8 || y < 1) ? false : true;
+  return (x > 8 || x < 1 || y > 'h' || y < 'a') ? false : true;
 };
-
-var columnsHelper = {
-                'a': 1,
-                'b': 2,
-                'c': 3,
-                'd': 4,
-                'e': 5,
-                'f': 6,
-                'g': 7,
-                'h': 8
-              };
 
 var checkUp, checkDown, checkLeft, checkRight, checkUpperRightDiag, checkUpperLeftDiag, checkLowerRightDiag, checkLowerLeftDiag;
 
-var highlight = function(x, y) {
+var highlightPiece = function(x, y) {
+  document.getElementById(y + x).setAttribute('style', 'background-color: #E5D352');
+};
+var highlightMoves = function(x, y) {
   document.getElementById(y + x).setAttribute('style', 'background-color: #D2D7DF');
-}
+};
 
 var moves = {
-  rook: function(x, y) {
+  rook: function(x, y, player) {
     // xy is current position
-    var temp = null
+    highlightPiece(x, y);
+    var temp = null;
+
     // check down
     var currentRow = x - 1;
     while (withinBounds(currentRow, y)) {
-      highlight(currentRow, y);
+      temp = document.getElementById(y + currentRow);
+      if (temp.innerText !== y + currentRow) {
+        if (temp.innerText[0] === 'b') {
+          highlightMoves(currentRow, y);
+        }
+        break;
+      } 
+      highlightMoves(currentRow, y);
       currentRow--;
     }
+
     // check up
     currentRow = x + 1;
     while (withinBounds(currentRow, y)) {
-      highlight(currentRow, y);
+      temp = document.getElementById(y + currentRow);
+      if (temp.innerText !== y + currentRow) {
+        if (temp.innerText[0] === 'b') {
+          highlightMoves(currentRow, y);
+        }
+        break;
+      } 
+      highlightMoves(currentRow, y);
       currentRow++;
     }
+
     // check right
-    var columnHelper = String.fromCharCode(y.charCodeAt(0) + 1);
-    while (columnsHelper[columnHelper] !== undefined) {
-      temp = document.getElementById(columnHelper + x);
-      if (temp.innerText !== columnHelper + x) {
+    var currentColumn = String.fromCharCode(y.charCodeAt(0) + 1);
+    while (withinBounds(x, currentColumn)) {
+      temp = document.getElementById(currentColumn + x);
+      if (temp.innerText !== currentColumn + x) {
         if (temp.innerText[0] === 'b') {
-          highlight(x, columnHelper);
+          highlightMoves(x, currentColumn);
         }
         break;
       }
-      highlight(x, columnHelper);
-      columnHelper = String.fromCharCode(columnHelper.charCodeAt(0) + 1);
+      highlightMoves(x, currentColumn);
+      currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) + 1);
     }
+    
     // check left
-    columnHelper = String.fromCharCode(y.charCodeAt(0) - 1);
-    while (columnsHelper[columnHelper] !== undefined) {
-      temp = document.getElementById(columnHelper + x);
-      if (temp.innerText !== columnHelper + x) {
+    currentColumn = String.fromCharCode(y.charCodeAt(0) - 1);
+    while (withinBounds(x, currentColumn)) {
+      temp = document.getElementById(currentColumn + x);
+      if (temp.innerText !== currentColumn + x) {
         if (temp.innerText[0] === 'b') {
-          highlight(x, columnHelper);         
+          highlightMoves(x, currentColumn);         
         }
         break;
       }
-      highlight(x, columnHelper);
-      columnHelper = String.fromCharCode(columnHelper.charCodeAt(0) - 1);
+      highlightMoves(x, currentColumn);
+      currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) - 1);
     }
   },
-  bishop: 'placeholder',
-  queen: function(x , y) {
-    moves.rook(x, y);
+
+  bishop: function(x, y, player) {
+    highlightPiece(x, y);
+    var temp = null;
+
+    // check upper right diagonal
+    var currentRow = x + 1;
+    var currentColumn = String.fromCharCode(y.charCodeAt(0) + 1);
+    while (withinBounds(currentRow, currentColumn)) {
+      temp = document.getElementById(currentColumn + currentRow);
+      if (temp.innerText !== currentColumn + currentRow) {
+        if (temp.innerText[0] === 'b') {
+          highlightMoves(currentRow, currentColumn);
+        }
+        break;
+      } 
+      highlightMoves(currentRow, currentColumn);
+      currentRow++;
+      currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) + 1);;
+    }
+
+    // check lower right diagonal
+    currentRow = x - 1;
+    currentColumn = String.fromCharCode(y.charCodeAt(0) + 1);
+    while (withinBounds(currentRow, currentColumn)) {
+      temp = document.getElementById(currentColumn + currentRow);
+      if (temp.innerText !== currentColumn + currentRow) {
+        if (temp.innerText[0] === 'b') {
+          highlightMoves(currentRow, currentColumn);
+        }
+        break;
+      } 
+      highlightMoves(currentRow, currentColumn);
+      currentRow--;
+      currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) + 1);;
+    }
+
+    // check lower left diagonal
+    currentRow = x - 1;
+    currentColumn = String.fromCharCode(y.charCodeAt(0) - 1);
+    while (withinBounds(currentRow, currentColumn)) {
+      temp = document.getElementById(currentColumn + currentRow);
+      if (temp.innerText !== currentColumn + currentRow) {
+        if (temp.innerText[0] === 'b') {
+          highlightMoves(currentRow, currentColumn);
+        }
+        break;
+      } 
+      highlightMoves(currentRow, currentColumn);
+      currentRow--;
+      currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) - 1);
+    }
+
+    // check upper left diagonal
+    currentRow = x + 1;
+    currentColumn = String.fromCharCode(y.charCodeAt(0) - 1);
+    while (withinBounds(currentRow, currentColumn)) {
+      temp = document.getElementById(currentColumn + currentRow);
+      if (temp.innerText !== currentColumn + currentRow) {
+        if (temp.innerText[0] === 'b') {
+          highlightMoves(currentRow, currentColumn);
+        }
+        break;
+      } 
+      highlightMoves(currentRow, currentColumn);
+      currentRow++;
+      currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) - 1);;
+    }
   },
-  pawn: 'placeholder',
-  knight: 'placeholder',
-  king: 'placeholder'
+
+  queen: function(x , y, player) {
+    moves.rook(x, y, player);
+    moves.bishop(x, y, player);
+  },
+
+  pawn: function(x, y, player) {
+
+  },
+
+  knight: function(x, y, player) {
+
+  },
+
+  king: function(x, y, player) {
+
+  }
 };
 
