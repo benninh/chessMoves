2cbde538aa626e6484ee5f6e7d46ded773ae8d22
Benson Ninh <bensonninh@gmail.com>
2017-04-11 13:48:27 -0700

    initialize a helper function file with a check to see whether or not a movement would is in bounds

---
+++ pieceMovementHelpers.js
@@ -0,0 +1,12 @@
+var withinBounds = function(x, y) {
+  return (x > 8 || x < 0 || y > 8 || y < 0) ? false : true;
+};
+
+var moves = {
+  pawn: 'placeholder',
+  rook: 'placeholder',
+  knight: 'placeholder',
+  bishop: 'placeholder',
+  queen: 'placeholder',
+  king: 'placeholder'
+};
