fb4016dc89fb17f6be43cb2ebb5e0b8c5100d172
Benson Ninh <bensonninh@gmail.com>
2017-04-11 15:37:24 -0700

    complete movement functionality of rooks currently ignores any check to see if player's king is in check

--- pieceMovementHelpers.js
+++ pieceMovementHelpers.js
@@ -1,13 +1,73 @@
 var withinBounds = function(x, y) {
-  return (x > 8 || x < 0 || y > 8 || y < 0) ? false : true;
+  return (x > 8 || x < 1 || y > 8 || y < 1) ? false : true;
 };
 
+var columnsHelper = {
+                'a': 1,
+                'b': 2,
+                'c': 3,
+                'd': 4,
+                'e': 5,
+                'f': 6,
+                'g': 7,
+                'h': 8
+              };
+
+var checkUp, checkDown, checkLeft, checkRight, checkUpperRightDiag, checkUpperLeftDiag, checkLowerRightDiag, checkLowerLeftDiag;
+
+var highlight = function(x, y) {
+  document.getElementById(y + x).setAttribute('style', 'background-color: #D2D7DF');
+}
+
 var moves = {
-  pawn: 'placeholder',
-  rook: 'placeholder',
-  knight: 'placeholder',
+  rook: function(x, y) {
+    // xy is current position
+    var temp = null
+    // check down
+    var currentRow = x - 1;
+    while (withinBounds(currentRow, y)) {
+      highlight(currentRow, y);
+      currentRow--;
+    }
+    // check up
+    currentRow = x + 1;
+    while (withinBounds(currentRow, y)) {
+      highlight(currentRow, y);
+      currentRow++;
+    }
+    // check right
+    var columnHelper = String.fromCharCode(y.charCodeAt(0) + 1);
+    while (columnsHelper[columnHelper] !== undefined) {
+      temp = document.getElementById(columnHelper + x);
+      if (temp.innerText !== columnHelper + x) {
+        if (temp.innerText[0] === 'b') {
+          highlight(x, columnHelper);
+        }
+        break;
+      }
+      highlight(x, columnHelper);
+      columnHelper = String.fromCharCode(columnHelper.charCodeAt(0) + 1);
+    }
+    // check left
+    columnHelper = String.fromCharCode(y.charCodeAt(0) - 1);
+    while (columnsHelper[columnHelper] !== undefined) {
+      temp = document.getElementById(columnHelper + x);
+      if (temp.innerText !== columnHelper + x) {
+        if (temp.innerText[0] === 'b') {
+          highlight(x, columnHelper);         
+        }
+        break;
+      }
+      highlight(x, columnHelper);
+      columnHelper = String.fromCharCode(columnHelper.charCodeAt(0) - 1);
+    }
+  },
   bishop: 'placeholder',
-  queen: 'placeholder',
+  queen: function(x , y) {
+    moves.rook(x, y);
+  },
+  pawn: 'placeholder',
+  knight: 'placeholder',
   king: 'placeholder'
 };
 
