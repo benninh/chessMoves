0b332d45d5c6ae9634983487dc8dd2f01f92f24d
benninh <bensonninh@gmail.com>
2017-04-14 18:07:05 -0700

    complete functionality of adding pieces to the board

--- addPiece.js
+++ addPiece.js
@@ -1,9 +1,28 @@
-var addPiece = function() {
-	var piece = document.getElementById('selectPiece').value;
-	var player = document.getElementById('selectPlayer').value;
-	var col = document.getElementById('selectColumn').value;
-	var row = document.getElementById('selectRow').value;
+var currentBoard;
 
+var addPiece = function(piece, player, col, row) {
+	var piece = piece || document.getElementById('selectPiece').value;
+	var player = player || document.getElementById('selectPlayer').value;
+	var col = col || document.getElementById('selectColumn').value;
+	var row = row || document.getElementById('selectRow').value;
 	var position = document.getElementById(col + row);
-	position.innerText(player + piece);
+	var boardContainer = document.getElementById('boardContainer');
+
+	position.innerText = player + piece;
+	
+	currentBoard = currentBoard || document.getElementById('board').cloneNode(true);
+
+	position.onmouseover = function() {
+													moves[piece](row, col, player);
+												}
+	
+	position.onmouseout = function() {
+													boardContainer.replaceChild(currentBoard, document.getElementById('board'));
+													// boardContainer.removeChild(document.getElementById('board'));
+													// boardContainer.appendChild(currentBoard);
+													addPiece(piece, player, col, row);
+												}
+	currentBoard = document.getElementById('board').cloneNode(true);
+	
+	return;
 }
