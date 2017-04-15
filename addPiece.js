d7ac21763aa67ed952ffe716cd5416cc64ba7c7f
benninh <bensonninh@gmail.com>
2017-04-14 18:19:32 -0700

    fix bug by converting row string to number

--- addPiece.js
+++ addPiece.js
@@ -4,7 +4,7 @@ var addPiece = function(piece, player, col, row) {
 	var piece = piece || document.getElementById('selectPiece').value;
 	var player = player || document.getElementById('selectPlayer').value;
 	var col = col || document.getElementById('selectColumn').value;
-	var row = row || document.getElementById('selectRow').value;
+	var row = row || Number(document.getElementById('selectRow').value);
 	var position = document.getElementById(col + row);
 	var boardContainer = document.getElementById('boardContainer');
 
@@ -12,7 +12,16 @@ var addPiece = function(piece, player, col, row) {
 	
 	currentBoard = currentBoard || document.getElementById('board').cloneNode(true);
 
+	/*TEST*/
+	document.getElementById('e7').innerText = 'bKing';
+	var testrow = 7;
+	var testcol = 'e';
+	var testplayer = 'b';
+	moves.King(testrow, testcol, testplayer);
+	/*    */
+
 	position.onmouseover = function() {
+													console.log(' row: ', typeof row, ' col: ', typeof col, ' player: ', typeof player);
 													moves[piece](row, col, player);
 												}
 	
