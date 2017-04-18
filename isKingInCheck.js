43577df701435c43912af3b4164e491387a0f5cd
benninh <bensonninh@gmail.com>
2017-04-17 21:09:15 -0700

    alter the check the fix a number of performance bugs implement check against enemy knight pieces

--- isKingInCheck.js
+++ isKingInCheck.js
@@ -11,27 +11,34 @@ var isKingInCheck = function(x, y, player) {
 
   // only pass in x and y when checking for king's moves.
   // king stays stationary otherwise
-  x = x || kingToVerify.row;
-  y = y || kingToVerify.col;
+  x = x === null ? kingToVerify.row : x;
+  y = y === null ? kingToVerify.col : y;
 
-	// var upOne = x + 1;
- //  var downOne = x - 1;
- //  var upTwo = x + 2;
- //  var downTwo = x - 2;
+  var upOne = x + 1;
+  var downOne = x - 1;
+  var upTwo = x + 2;
+  var downTwo = x - 2;
 
- //  var currentColumn = String.fromCharCode(y.charCodeAt(0));
- //  var leftOne = String.fromCharCode(y.charCodeAt(0) - 1);
- //  var rightOne = String.fromCharCode(y.charCodeAt(0) + 1);
- //  var leftTwo = String.fromCharCode(y.charCodeAt(0) - 2);
- //  var rightTwo = String.fromCharCode(y.charCodeAt(0) + 2);
+  var leftOne = String.fromCharCode(y.charCodeAt(0) - 1);
+  var rightOne = String.fromCharCode(y.charCodeAt(0) + 1);
+  var leftTwo = String.fromCharCode(y.charCodeAt(0) - 2);
+  var rightTwo = String.fromCharCode(y.charCodeAt(0) + 2);
 
   var cell;
+  var temp = false;
   // down
   var currentRow = x - 1;
   while (withinBounds(currentRow, y)) {
   	cell = document.getElementById(y + currentRow);
-    if (checkCellForPiece(currentRow, y) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    if (checkCellForPiece(currentRow, y) && cell.innerText[0] !== player) {
+      if (currentRow === x - 1) {
+      	temp = ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    	}
     }
     currentRow--;
   }
@@ -40,8 +47,17 @@ var isKingInCheck = function(x, y, player) {
   currentRow = x + 1;
   while (withinBounds(currentRow, y)) {
   	cell = document.getElementById(y + currentRow);
-    if (checkCellForPiece(currentRow, y) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    if (checkCellForPiece(currentRow, y) && cell.innerText[0] !== player) {
+    	console.log(currentRow)
+    	if (currentRow === x + 1) {
+      	temp = ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		console.log(temp);
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    	}
     } 
     currentRow++;
   }
@@ -50,8 +66,15 @@ var isKingInCheck = function(x, y, player) {
   var currentColumn = String.fromCharCode(y.charCodeAt(0) + 1);
   while (withinBounds(x, currentColumn)) {
   	cell = document.getElementById(currentColumn + x);
-    if (checkCellForPiece(x, currentColumn) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    if (checkCellForPiece(x, currentColumn) && cell.innerText[0] !== player) {
+			if (currentColumn === rightOne) {
+      	temp = ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    	}
     }
     currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) + 1);
   }
@@ -60,8 +83,15 @@ var isKingInCheck = function(x, y, player) {
   currentColumn = String.fromCharCode(y.charCodeAt(0) - 1);
   while (withinBounds(x, currentColumn)) {
   	cell = document.getElementById(currentColumn + x);
-    if (checkCellForPiece(x, currentColumn) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    if (checkCellForPiece(x, currentColumn) && cell.innerText[0] !== player) {
+      if (currentColumn === leftOne) {
+      	temp = ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Rook' || cell.innerText.substring(1) === 'Queen'));
+    	}
     }
     currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) - 1);
   }
@@ -71,8 +101,15 @@ var isKingInCheck = function(x, y, player) {
   currentColumn = String.fromCharCode(y.charCodeAt(0) + 1);
   while (withinBounds(currentRow, currentColumn)) {
   	cell = document.getElementById(currentColumn + currentRow);
-    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'))
+    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player) {
+    	if (currentColumn === rightOne && currentRow === upOne) {
+      	temp = ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'));
+    	}
     } 
     currentRow++;
     currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) + 1);;
@@ -83,8 +120,15 @@ var isKingInCheck = function(x, y, player) {
   currentColumn = String.fromCharCode(y.charCodeAt(0) + 1);
   while (withinBounds(currentRow, currentColumn)) {
   	cell = document.getElementById(currentColumn + currentRow);
-    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-    	return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'))
+    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player) {
+    	if (currentColumn === rightOne && currentRow === downOne) {
+      	temp = ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'));
+    	}
     }
     currentRow--;
     currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) + 1);;
@@ -95,8 +139,15 @@ var isKingInCheck = function(x, y, player) {
   currentColumn = String.fromCharCode(y.charCodeAt(0) - 1);
   while (withinBounds(currentRow, currentColumn)) {
   	cell = document.getElementById(currentColumn + currentRow);
-    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'))
+    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player) {
+    	if (currentColumn === leftOne && currentRow === downOne) {
+      	temp = ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'));
+    	}
     } 
     currentRow--;
     currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) - 1);
@@ -107,12 +158,76 @@ var isKingInCheck = function(x, y, player) {
   currentColumn = String.fromCharCode(y.charCodeAt(0) - 1);
   while (withinBounds(currentRow, currentColumn)) {
   	cell = document.getElementById(currentColumn + currentRow);
-    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player && cell.innerText.substring(1) !== 'King') {
-      return (cell.innerText[0] !== player && (cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'))
+    if (checkCellForPiece(currentRow, currentColumn) && cell.innerText[0] !== player) {
+    	if (currentColumn === leftOne && currentRow === upOne) {
+      	temp = ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen' || cell.innerText.substring(1) === 'King'));
+    		if (temp) {
+    			return temp;
+    		}
+    	} else {
+      	return ((cell.innerText.substring(1) === 'Bishop' || cell.innerText.substring(1) === 'Queen'));
+    	}
     } 
     currentRow++;
     currentColumn = String.fromCharCode(currentColumn.charCodeAt(0) - 1);;
   }
 
+  // knight check
+	// up 2 left 1
+	if (withinBounds(upTwo, leftOne)) {
+		cell = document.getElementById(leftOne + upTwo);
+    if (checkCellForPiece(upTwo, leftOne) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // up 2 right 1
+  if (withinBounds(upTwo, rightOne)) {
+		cell = document.getElementById(rightOne + upTwo);
+    if (checkCellForPiece(upTwo, rightOne) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // up 1 right 2
+  if (withinBounds(upOne, rightTwo)) {
+		cell = document.getElementById(rightTwo + upOne);
+    if (checkCellForPiece(upOne, rightTwo) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // up 1 left 2
+  if (withinBounds(upOne, leftTwo)) {
+		cell = document.getElementById(leftTwo + upOne);
+    if (checkCellForPiece(upOne, leftTwo) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // down 2 left 1
+  if (withinBounds(downTwo, leftOne)) {
+		cell = document.getElementById(leftOne + downTwo);
+    if (checkCellForPiece(downTwo, leftOne) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // down 2 right 1
+  if (withinBounds(downTwo, rightOne)) {
+		cell = document.getElementById(rightOne + downTwo);
+    if (checkCellForPiece(downTwo, rightOne) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // down 1 left 2
+  if (withinBounds(downOne, leftTwo)) {
+		cell = document.getElementById(leftTwo + downOne);
+    if (checkCellForPiece(downOne, leftTwo) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
+  // down 1 right 2
+  if (withinBounds(downOne, rightTwo)) {
+		cell = document.getElementById(rightTwo + downOne);
+    if (checkCellForPiece(downOne, rightTwo) && cell.innerText[0] !== player) {
+    	return (cell.innerText.substring(1) === 'Knight');
+    }
+  }
   return false;
 }
